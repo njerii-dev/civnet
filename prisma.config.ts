5e2ad4d21054cedca694c764e4c520f841ceb10f
njerii-dev <njeriijoy216@gmail.com>
2026-01-28 15:28:00 +0300

    Remove prisma.config.ts configuration file
    Deleted the prisma.config.ts file, which contained the Prisma datasource configuration. This may be part of a migration or cleanup of unused configuration files.

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // Paste your real link from Neon between the quotes below
-    url: "postgresql://alex:password@ep-cool-dark-123.aws.neon.tech/neondb?sslmode=require",
-  },
-});
